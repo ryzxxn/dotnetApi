e4ca9c77b1d275d8b38096db36a7815eb8885db4
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-07-08 10:58:54 +0530

    sdfsdf

--- obj/Debug/net8.0/dotnetApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b75a52f0effb4dff823127d16efeb180915722")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8784b1961b7f97050c457ec6dac8652d481d2828")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
